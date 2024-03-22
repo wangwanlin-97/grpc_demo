7d73911b5ab91018cc6d81bc05537295b67efaed
wangwanlin-97 <1663037038@qq.com>
2024-03-22 18:55:49 +0800

    Add spark_web

--- src/client/chat_client.py
+++ src/client/chat_client.py
@@ -1,15 +1,20 @@
 import grpc
 from proto import messages_pb2_grpc
 from proto.messages_pb2 import ChatRequest
+from configparser import ConfigParser
+import sys
 
-def run():
+def run():   
     with grpc.insecure_channel("localhost:50051") as channel:
         chat_stub = messages_pb2_grpc.ChatStub(channel)
         while True:
-            q=input("user:")
-            response = chat_stub.chat(iter([ChatRequest(message=q,name="wang")]),timeout=1)
+            q=input("\nuser:")
+            response = chat_stub.chat(iter([ChatRequest(message=q,name="wang")]))
+            print("REPLY:")
             for message in response:
-                print(f"REPLY:{message.message}\n")
+                sys.stdout.flush()
+                print(f"{message.message}",end="")
+                
 
 if __name__ == "__main__":
     run()
